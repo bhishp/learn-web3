83bb11ae4abab2316dfe70c06f1ac7b167a9c27c
bhishp <bhishp@users.noreply.github.com>
2022-07-10 12:33:12 +0100

    - add chakra and web3-react - support connecting via metamask connectors and show connection info

--- 01.eth-fees/src/App.tsx
+++ 01.eth-fees/src/App.tsx
@@ -1,19 +1,90 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Box, Button, Container, Flex, Heading, Stat, StatLabel, StatNumber, VStack } from "@chakra-ui/react";
+import { metaMask, metaMaskHooks } from "./web3/connectors";
+
+enum ChainID {
+  MAINNET = 1,
+  Ropsten = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  LOCAL = 1337,
+}
+
+const CHAINS = {
+  [ChainID.MAINNET]: {
+    name: "Mainnet",
+  },
+  [ChainID.Ropsten]: {
+    name: "Ropsten",
+  },
+  [ChainID.RINKEBY]: {
+    name: "Rinkeby",
+  },
+  [ChainID.GÖRLI]: {
+    name: "Görli",
+  },
+  [ChainID.LOCAL]: {
+    name: "Local",
+  },
+};
+
+const { useIsActive, useAccounts, useChainId } = metaMaskHooks;
 
 function App() {
+  const [error, setError] = useState<Error | null>(null);
+  const isActive = useIsActive();
+  const accounts = useAccounts();
+  const connectedChainId = useChainId();
+  const connectedChainName =
+    connectedChainId === undefined ? "-" : CHAINS[connectedChainId as ChainID]?.name || "Unknown Chain";
+
+  const connectWallet = async () => {
+    try {
+      await metaMask.activate();
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e);
+      } else {
+        setError(new Error("Unknown error"));
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container centerContent py="8">
+      <VStack spacing="8">
+        <header>
+          <Heading as="h1" size="4xl">
+            Eth Fees
+          </Heading>
+        </header>
+        <VStack alignItems="start" spacing="2">
+          <Stat>
+            <StatLabel>Connection</StatLabel>
+            <StatNumber>
+              {error ? `Error 🔴 ${error.message}` : isActive ? "Connected 🟢" : "Not connected ⚪"}️
+            </StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>Chain</StatLabel>
+            <StatNumber>{connectedChainName}</StatNumber>
+          </Stat>
+          <Stat>
+            <StatLabel>Connected Wallet</StatLabel>
+            <StatNumber>{accounts?.toString() || "-"}</StatNumber>
+            {/*<StatHelpText>Feb 12 - Feb 28</StatHelpText>*/}
+          </Stat>
+          <Stat>
+            <StatLabel>Number of Transactions</StatLabel>
+            <StatNumber>-</StatNumber>
+            {/*<StatHelpText>Feb 12 - Feb 28</StatHelpText>*/}
+          </Stat>
+        </VStack>
+        <Button disabled={isActive} colorScheme="blue" onClick={connectWallet}>
+          Connect Wallet
+        </Button>
+      </VStack>
+    </Container>
   );
 }
 
--- 01.eth-fees/src/index.tsx
+++ 01.eth-fees/src/index.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3ReactProviderProps } from "@web3-react/core/dist/provider";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { metaMask, metaMaskHooks } from "./web3/connectors";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+//     "@ethersproject/bignumber": "^5.6.0",
+//     "@ethersproject/experimental": "^5.6.0",
+//     "@ethersproject/providers": "^5.6.0",
+//     "@ethersproject/units": "^5.6.0",
+
+const connectors: Web3ReactProviderProps["connectors"] = [[metaMask, metaMaskHooks]];
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <Web3ReactProvider connectors={connectors}>
+        <App />
+      </Web3ReactProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
